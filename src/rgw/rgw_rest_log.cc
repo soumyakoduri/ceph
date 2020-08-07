0f2ee5c7418f98061e3942e6bcd690fedbb74225
Adam C. Emerson <aemerson@redhat.com>
2020-08-07 11:28:31 -0400

    rgw: Fixup listing in MDLog
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_rest_log.cc
+++ src/rgw/rgw_rest_log.cc
@@ -43,7 +43,6 @@ void RGWOp_MDLog_List::execute() {
   string   max_entries_str = s->info.args.get("max-entries");
   string   marker = s->info.args.get("marker"),
            err;
-  void    *handle;
   unsigned shard_id, max_entries = LOG_CLASS_LIST_MAX_ENTRIES;
 
   if (s->info.args.exists("start-time") ||
@@ -83,12 +82,8 @@ void RGWOp_MDLog_List::execute() {
 
   RGWMetadataLog meta_log{s->cct, store->svc()->zone, store->svc()->cls, period};
 
-  meta_log.init_list_entries(shard_id, {}, {}, marker, &handle);
-
-  http_ret = meta_log.list_entries(handle, max_entries, entries,
+  http_ret = meta_log.list_entries(shard_id, max_entries, marker, entries,
                                    &last_marker, &truncated);
-
-  meta_log.complete_list_entries(handle);
 }
 
 void RGWOp_MDLog_List::send_response() {
@@ -104,9 +99,7 @@ void RGWOp_MDLog_List::send_response() {
   s->formatter->dump_bool("truncated", truncated);
   {
     s->formatter->open_array_section("entries");
-    for (list<cls_log_entry>::iterator iter = entries.begin();
-	 iter != entries.end(); ++iter) {
-      cls_log_entry& entry = *iter;
+    for (const auto& entry : entries) {
       store->ctl()->meta.mgr->dump_log_entry(entry, s->formatter);
       flusher.flush();
     }
