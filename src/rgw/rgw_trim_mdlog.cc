75f0b52dc89a075895dabc3a68a713d638fb6b2f
Soumya Koduri <skoduri@redhat.com>
2020-09-06 01:20:34 +0530

    rgw: Add a couroutine for RGWMetadataLogTrim operation
    Define a new co-routine which acts as wrapper for RGWMetadataLog
    trim operation. This coroutine is to be returned for master and
    peer trim operations. 'exclusive' field determines whether trim should
    include marker (in case of master) or not (for peers).
    
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/rgw/rgw_trim_mdlog.cc
+++ src/rgw/rgw_trim_mdlog.cc
@@ -270,10 +270,8 @@ bool MetaMasterTrimShardCollectCR::spawn_next()
         << " at marker=" << stable
         << " last_trim=" << last_trim
         << " realm_epoch=" << sync_status.sync_info.realm_epoch << dendl;
-    RGWCoroutine* master_trim_cr = mdlog->master_trim_cr(shard_id, stable, &last_trim);
-    if (master_trim_cr) {
-      spawn(master_trim_cr, false);
-    }
+
+    spawn(mdlog->master_trim_cr(shard_id, stable, &last_trim) , false);
 
     shard_id++;
     return true;
@@ -466,10 +464,7 @@ int MetaPeerTrimShardCR::operate()
         << " at marker=" << stable
         << " last_trim=" << *last_trim << dendl;
     yield {
-      RGWCoroutine* peer_trim_cr = mdlog->peer_trim_cr(shard_id, stable);
-      if (peer_trim_cr) {
-        call(peer_trim_cr);
-      }
+      call(mdlog->peer_trim_cr(shard_id, stable));
     }
     if (retcode < 0 && retcode != -ENODATA) {
       ldpp_dout(env.dpp, 1) << "failed to trim mdlog shard " << shard_id
