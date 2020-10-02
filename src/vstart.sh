b422197eb5ba16200592f3fb6618bfefdbb3d1b0
Soumya Koduri <skoduri@redhat.com>
2020-10-02 23:11:22 +0530

    rgw: check if the object is already tiered
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -117,7 +117,7 @@ fi
 [ -z "$CEPH_NUM_MON" ] && CEPH_NUM_MON=3
 [ -z "$CEPH_NUM_OSD" ] && CEPH_NUM_OSD=3
 [ -z "$CEPH_NUM_MDS" ] && CEPH_NUM_MDS=3
-[ -z "$CEPH_NUM_MGR" ] && CEPH_NUM_MGR=1
+[ -z "$CEPH_NUM_MGR" ] && CEPH_NUM_MGR=0
 [ -z "$CEPH_NUM_FS"  ] && CEPH_NUM_FS=1
 [ -z "$CEPH_MAX_MDS" ] && CEPH_MAX_MDS=1
 [ -z "$CEPH_NUM_RGW" ] && CEPH_NUM_RGW=0
